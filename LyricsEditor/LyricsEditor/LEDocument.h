d3e8634798971b4376e3104b3440b5a91fce7bcf
ziv yuan <ziv.yuan@gmail.com>
2012-12-17 10:42:00 +0800

    创建歌词编辑器子项目

---
+++ LyricsEditor/LyricsEditor/LEDocument.h
@@ -0,0 +1,13 @@
+//
+//  LEDocument.h
+//  LyricsEditor
+//
+//  Created by ziv yuan on 12-12-17.
+//  Copyright (c) 2012年 ziv yuan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LEDocument : NSDocument
+
+@end
---
+++ LyricsEditor/LyricsEditor/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  LyricsEditor
+//
+//  Created by ziv yuan on 12-12-17.
+//  Copyright (c) 2012年 ziv yuan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
